48814778c4aa01a9c8eea73ef441ba719b1f7f95
askn <tc535mr2@gmail.com>
2021-04-15 20:08:42 +0900

    nycを追加

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
+      require('../routes/index');
+      require('../routes/users');
       assert.strictEqual([1, 2, 3].indexOf(4), -1);
     });
   });
